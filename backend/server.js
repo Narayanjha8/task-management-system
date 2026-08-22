ef3cf2d85c0591da04d89563c71c43bad55c841c
Narayanjha8 <nnarayankumarjha8@gmail.com>
2026-08-22 19:58:10 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -249,7 +249,7 @@ const PORT = process.env.PORT || 5001
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected')
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`)
     })
   })
